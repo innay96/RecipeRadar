06a1afcabfad9c113d3c44b867e98c6e0d7a1bcd
Inna yakubov <inna.yakubov1996@gmail.com>
2018-12-31 18:40:04 +0200

    added the pending database

--- app/src/main/java/com/example/innapc/reciperadar/AddingActivity.java
+++ app/src/main/java/com/example/innapc/reciperadar/AddingActivity.java
@@ -4,21 +4,23 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.firebase.client.Firebase;
-
-import java.util.ArrayList;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AddingActivity extends AppCompatActivity {
     private EditText category;
     private EditText recipeName;
+    private EditText prepare;
+    private EditText ingredient;
     private Button send;
 
-    private ArrayList<String> recipesList =  new ArrayList<>(); // contains the value of the recipe (the ingredients)
-
-    private AdminActivity admin = new AdminActivity();
+    private DatabaseReference recipesDatabase; // connector between the app and the database
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,62 +29,51 @@ public class AddingActivity extends AppCompatActivity {
         Firebase.setAndroidContext(this);
         category = findViewById(R.id.categoryText);
         recipeName = findViewById(R.id.recipeNameText);
+        prepare = findViewById(R.id.prepareText);
+        ingredient = findViewById(R.id.ingredientsText);
         send = findViewById(R.id.sendBtn);
+
 
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (v == send){
-                    admin.addRecipe(recipesList, category, recipeName); // sends to the admin to get an approval
+                    recipesDatabase = FirebaseDatabase.getInstance().getReference().child("Pending").child(category.getText().toString()).child(recipeName.getText().toString());
+                    recipesDatabase.child("Ingredients").setValue(ingredient.getText().toString())
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(AddingActivity.this,"Item was added successfully!!",
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(Exception e) {
+                                    Toast.makeText(AddingActivity.this,"Failed adding item",
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            });
+                    recipesDatabase.child("Prepare").setValue(prepare.getText().toString())
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(AddingActivity.this,"Item was added successfully!!",
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(Exception e) {
+                                    Toast.makeText(AddingActivity.this,"Failed adding item",
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            });
                 }
             }
         });
-    }
 
-    /**
-     * checks which checkbox was checked and adds it to the recipesList
-     * @param view
-     */
-    public void selectItem(View view){
-        boolean checked = ((CheckBox) view).isChecked();
-        switch(view.getId()){
-            case R.id.eggsCheckBox:
-                if(checked)
-                    recipesList.add("Eggs");
-                else
-                    recipesList.remove("Eggs");
-                break;
-            case R.id.milkCheckBox:
-                if(checked)
-                    recipesList.add("Milk");
-                else
-                    recipesList.remove("Milk");
-                break;
-            case R.id.peanutsCheckBox:
-                if(checked)
-                    recipesList.add("Peanuts");
-                else
-                    recipesList.remove("Peanuts");
-                break;
-            case R.id.mushroomsCheckBox:
-                if(checked)
-                    recipesList.add("Mushrooms");
-                else
-                    recipesList.remove("Mushrooms");
-                break;
-            case R.id.sugarCheckBox:
-                if(checked)
-                    recipesList.add("Sugar");
-                else
-                    recipesList.remove("Sugar");
-                break;
-            case R.id.glutenCheckBox:
-                if(checked)
-                    recipesList.add("Gluten");
-                else
-                    recipesList.remove("Gluten");
-                break;
-        }
 
     }
+
 }
